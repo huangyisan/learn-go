ee7df81f0dde0f6f4115b031d56d6b5433e025a6
huangyisan <ahuangyisan@live.cn>
2020-05-20 00:29:06 +0800

    update

--- 常用的内置函数/func_date_time/func_dateAndtime.go
+++ 常用的内置函数/func_date_time/func_dateAndtime.go
@@ -13,6 +13,11 @@ func main() {
 	fmt.Printf("now的值为%v, 类型为%T\n", now, now)
 
 	//2. 获取年月日 时分秒
-	fmt.Printf("年%v, 月%v, 日%v, 时%v, 分%v, 秒%v", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+	fmt.Printf("年%v, 月%v, 日%v, 时%v, 分%v, 秒%v\n", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+
+	//	3. 格式化,和其他语言不一样,不是使用yyyy mm hh,而是使用固定的数值 2006/01/02 15:04:05, go语言中通过不同的数值来区分是属于什么字段.
+	// 相当有意思 https://stackoverflow.com/questions/45160822/what-does-20060102150405-mean
+	fmt.Println(now.Format("15:04:05 2006/01/02"))
+
 
 }
