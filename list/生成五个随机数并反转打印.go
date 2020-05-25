80d869b52a00accf80bf59039a39363e7a5d0e26
huangyisan <ahuangyisan@live.cn>
2020-05-26 00:23:55 +0800

    update

--- list/生成五个随机数并反转打印.go
+++ list/生成五个随机数并反转打印.go
@@ -15,22 +15,23 @@ import (
 func main() {
 	// 定义一个长度为5的数组
 	var intArry [5]int
+	const listLength = len(intArry)
 
 	// 为了每次的seed值不一样, 使用时间戳进行改变seed.
 	rand.Seed(time.Now().Unix())
 
 	// 获得数组长度的一般.
-	halfLength := len(intArry) / 2
+	halfLength := listLength / 2
 
 	// 将随机数放入数组中
-	for i:=0; i< len(intArry); i++ {
+	for i:=0; i< listLength; i++ {
 		intArry[i] = rand.Intn(100) //0<=x<100, 无法取到100
 	}
 	fmt.Println("原先数组为",intArry)
 	//进行交换,交换次数为长度的一半,向下取整.
 
 	for l:=0; l < halfLength;l++ {
-		intArry[l], intArry[len(intArry)-1-l] = intArry[len(intArry)-1-l], intArry[l]
+		intArry[l], intArry[listLength-1-l] = intArry[listLength-1-l], intArry[l]
 
 	}
 
