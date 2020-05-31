a17f6a5e2346cbe677dc90f28bdff2a9a69073cc
huangyisan <anonymousyisan@gmail.com>
2020-06-01 05:55:11 +0800

    update

--- 进阶部分/goroutine和channel/3. channel的引出.go
+++ 进阶部分/goroutine和channel/3. channel的引出.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
 // 使用goroutine实现1-200的阶乘，并且放入map中，最后打印显示。
 
+// 1. 编写函数，计算各个数的阶乘，并放入map中。
+// 2. 启动协程多个， 统计的结果放入map。
+// 3. map应该是全局，这样才能让协程都来使用。
+
+var (
+	myMap = make(map[int]int, 10)
+)
+
+
+// 计算n的阶乘，将结果放入myMap中
+func test(n int) {
+	res := 1
+	for i :=1; i<=n; i++ {
+		res *= i
+	}
+
+	// 将res结果放入myMap
+	myMap[n] = res
+}
+
+// 存在的问题
+/*
+写入map存在竞争，不安全。
+可能主线程比协程提前结束，从而导致协程被迫停止。
+ */
+func main() {
+	//启动多个协程
+	for i := 1; i<=200; i++ {
+		go test(i)
+	}
+
+	// 防止主进程先结束，手工10s等待。
+	time.Sleep(time.Second * 10)
+
+	for k,v := range myMap {
+		fmt.Printf("key is %v, value is %v\n",k,v)
+	}
+
+}
---
+++ 进阶部分/goroutine和channel/4. goroutine的通讯-channel方式.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+1. channel的本质是一个数据结构-队列
+2. 数据是先进先出
+3. 线程安全，多goroutine访问时，不需要加锁，就是说channel本身就是线程安全的
+4. channel是有类型的，一个string的channel只能存放string。
+ */
+
+ // 使用全局变量加锁同步
+var (
+	myMap = make(map[int]int64, 10)
+
+	// lock 是一个全局的互斥锁
+	lock sync.Mutex
+)
+
+
+// 计算n的阶乘，将结果放入myMap中
+
+func test(n int) {
+	res := 1
+	for i :=1; i<=n; i++ {
+		res *= i
+	}
+
+	// 将res结果放入myMap
+	// 这个动作执行之前加锁
+	lock.Lock()
+	myMap[n] = int64(res)
+	// 执行完后解锁
+	lock.Unlock()
+}
+
+func main() {
+	//启动多个协程
+	for i := 1; i<=20; i++ {
+		go test(i)
+	}
+
+	// 防止主进程先结束，手工10s等待。
+	time.Sleep(time.Second * 10)
+
+	// 从程序设计上能知道10s肯定能结束所有协程，但主线程并不知道，因此底层还是可能会产生资源争夺，因此还是需要加入互斥锁来解决。
+	lock.Lock()
+	for k,v := range myMap {
+		fmt.Printf("key is %v, value is %v\n",k,v)
+	}
+	lock.Unlock()
+
+}
