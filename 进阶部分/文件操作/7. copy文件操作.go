d07a7b6d6e86702313671622c58d84ef28aa0bad
huangyisan <ahuangyisan@live.cn>
2020-06-03 20:37:04 +0800

    update

---
+++ 进阶部分/文件操作/7. copy文件操作.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"io"
+)
+
+// 使用io包的Copy函数实现.
+
+// 将Test文件copy到源目录,名称为Test-copy
+
+
+func CopyFile(ori string, dst string) (written int64, err error) {
+	// 打开源文件
+	srcFile, err := os.Open(ori)
+	defer srcFile.Close()
+	if err !=nil {
+		fmt.Println(err)
+	}
+	// 通过srcFile获取Reader
+	newReader := bufio.NewReader(srcFile)
+
+
+	// 打开目标文件, 因为目标文件可能不存在,所以用OpenFile函数
+	file , err := os.OpenFile(dst, os.O_WRONLY | os.O_CREATE, 0644)
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	newWriter := bufio.NewWriter(file)
+
+
+	count,err := io.Copy(newWriter, newReader)
+	newWriter.Flush()
+	return count, err
+}
+
+func main () {
+	dst := "/Users/huangyisan/Desktop/github/hys/learn-go/进阶部分/文件操作/Test-copy"
+	src := "/Users/huangyisan/Desktop/github/hys/learn-go/进阶部分/文件操作/Test"
+
+	// 调用函数进行copy
+	_, err := CopyFile(src, dst)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("copy成功")
+	}
+}
