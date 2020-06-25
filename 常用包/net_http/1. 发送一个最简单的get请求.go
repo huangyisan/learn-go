b0b2932b3c112b985e8f36602953f20c8a3e5ccd
huangyisan <anonymousyisan@gmail.com>
2020-06-25 17:02:52 +0800

    update

---
+++ 常用包/net_http/1. 发送一个最简单的get请求.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
+
+func httpGet(url string) (err error){
+	req, err := http.Get(url)
+	defer req.Body.Close()
+	fmt.Println(1)
+	if err != nil {
+		fmt.Println("get请求失败， %s", err.Error())
+		return
+	}
+
+	content, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("读取content失败")
+		return
+	}
+	fmt.Println(string(content))
+	return
+
+}
+
+func main() {
+	url := "https://httpbin.com/get"
+	httpGet(url)
+}
