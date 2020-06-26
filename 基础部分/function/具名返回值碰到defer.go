52029fa0bd90287a9e31e34a1063632bda615388
huangyisan <ahuangyisan@live.cn>
2020-06-26 23:18:23 +0800

    update

---
+++ 基础部分/function/具名返回值碰到defer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("b return:", b()) // 打印结果为 b return: 2
+}
+
+func b() (i int) {
+	defer func() {
+		i++
+		fmt.Println("b defer2:", i) // 打印结果为 b defer2: 2
+	}()
+	defer func() {
+		i++
+		fmt.Println("b defer1:", i) // 打印结果为 b defer1: 1
+	}()
+	return i // 或者直接 return 效果相同
+}
