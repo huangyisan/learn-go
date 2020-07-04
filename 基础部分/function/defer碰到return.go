3664d810fa42019bd94bebadc87c83674a7e867b
huangyisan <ahuangyisan@live.cn>
2020-07-04 16:55:15 +0800

    update

--- 基础部分/function/defer碰到return.go
+++ 基础部分/function/defer碰到return.go
@@ -3,15 +3,13 @@ package main
 import "fmt"
 
 func add(a int) (err error) {
-	defer fmt.Println("我是defer")
-
 	if a == 3{
 		fmt.Println("我是if")
 		return
 	}
+	defer fmt.Println("我是if return后面的defer")
 	return
 }
-
 
 func main() {
 	add(3)
--- 基础部分/function/具名返回值碰到defer.go
+++ 基础部分/function/具名返回值碰到defer.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 )
 
-func foo() (string) {
-	var result string
+func foo() (result string) {
 	result = "ccc"
 	defer func() {
 		result = "Change World" // change value at the very last moment
@@ -14,18 +13,5 @@ func foo() (string) {
 }
 
 func main() {
-	fmt.Println("b return:", b()) // 打印结果为 b return: 2
 	fmt.Println(foo())
-}
-
-func b() (i int) {
-	defer func() {
-		i++
-		fmt.Println("b defer2:", i) // 打印结果为 b defer2: 2
-	}()
-	defer func() {
-		i++
-		fmt.Println("b defer1:", i) // 打印结果为 b defer1: 1
-	}()
-	return i // 或者直接 return 效果相同
 }
