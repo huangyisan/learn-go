3bca3576b554721847e507b919af193ae3aa98a5
huangyisan <anonymousyisan@gmail.com>
2020-11-13 17:10:38 +0800

    关闭channel

---
+++ previous/进阶部分/goroutine和channel/3.5. 关闭channel.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// 关闭channel
+// close(channelName)
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <-1
+		}
+		//关闭channel
+		close(ch)
+	}()
+
+	for {
+		// <-ch 操作返回两个值，一个为channel里面的内容，一个为状态。
+		// 如果ok为true，则表示channel未关闭， 反之则表示channel关闭。
+		if num, ok := <-ch; ok == true {
+			fmt.Println("num = ", num)
+		} else{
+			break
+		}
+	}
+}
