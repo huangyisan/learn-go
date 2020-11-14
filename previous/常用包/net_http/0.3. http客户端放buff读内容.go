1de51ff20f6f998426a1c83c288db11881b28e02
huangyisan <anonymousyisan@gmail.com>
2020-11-14 15:46:08 +0800

    http客户端buff读取内容

---
+++ previous/常用包/net_http/0.3. http客户端放buff读内容.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		fmt.Println("Get error")
+		return
+	}
+
+	// 最后关闭body
+	defer resp.Body.Close()
+
+	// 创建4k缓存
+	buf := make([]byte, 4*1024)
+	var tmp string
+
+	// 不停读取，直到返回为0，表示没有数据了
+	for  {
+		// 往buff读
+		n, err := resp.Body.Read(buf)
+		// 此时读取完毕, 则break
+		if n == 0 {
+			break
+		}
+		
+
+		tmp += string(buf[:n])
+	}
+
+	fmt.Printf("content is %s \n", tmp)
+
+
+}
