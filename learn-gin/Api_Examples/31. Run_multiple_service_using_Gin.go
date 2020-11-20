311cf6d874c5e41c680f6fb6db2ecd609ad6215f
huangyisan <anonymousyisan@gmail.com>
2020-11-20 18:24:28 +0800

    Run multiple service using gin

---
+++ learn-gin/Api_Examples/31. Run_multiple_service_using_Gin.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net/http"
+	"time"
+)
+
+// 如果需要run多个gin service可以使用errgroup
+
+var g errgroup.Group
+
+// 定义多个router, 返回为一个Handler让
+func router01() http.Handler {
+	e := gin.New()
+	e.Use(gin.Recovery())
+	e.GET("/", func(c *gin.Context) {
+		c.JSON(
+			http.StatusOK,
+			gin.H{
+				"code":  http.StatusOK,
+				"error": "Welcome server 01",
+			},
+		)
+	})
+
+	return e
+}
+
+func router02() http.Handler {
+	e := gin.New()
+	e.Use(gin.Recovery())
+	e.GET("/", func(c *gin.Context) {
+		c.JSON(
+			http.StatusOK,
+			gin.H{
+				"code":  http.StatusOK,
+				"error": "Welcome server 02",
+			},
+		)
+	})
+
+	return e
+}
+
+func main() {
+	server01 := &http.Server{
+		Addr:         ":8080",
+		// 将函数传递给handler
+		Handler:      router01(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	server02 := &http.Server{
+		Addr:         ":8081",
+		Handler:      router02(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// GO派生一个goroutines，里面是一个匿名函数，返回error
+	g.Go(func() error {
+		// 这里是启动了实例的监听
+		err := server01.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+		return err
+	})
+
+	g.Go(func() error {
+		err := server02.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+		return err
+	})
+
+	// Wait等待执行完毕
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
+}
