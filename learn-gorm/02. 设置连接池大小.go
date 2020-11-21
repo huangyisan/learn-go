4b534522cbd48f9cfe4c9d257a09e524a912418e
huangyisan <anonymousyisan@gmail.com>
2020-11-22 00:11:34 +0800

    设置连接池大小

--- learn-gorm/02. 设置连接池大小.go
+++ learn-gorm/02. 设置连接池大小.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 )
 
 func main() {
@@ -19,6 +20,14 @@ func main() {
 	if err != nil {
 		return
 	}
+	// 设置连接池属性
+	// 最大空闲连接数
+	sqlDB.SetMaxIdleConns(10)
+	// 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+	// 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(10 * time.Hour)
+
 	if ok := sqlDB.Close(); ok == nil {
 		fmt.Println("数据库关闭成功")
 	}
