429a38e76b2c4993f59ac66b31a387f24acdb076
huangyisan <anonymousyisan@gmail.com>
2020-11-22 22:49:16 +0800

    initdb

--- go-api/main.go
+++ go-api/main.go
@@ -3,10 +3,39 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"goapi/router"
+	"os"
+	"goapi/model"
 )
 
+func initConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+}
+
 func main() {
+	// 读取yaml配置
+	initConfig()
+
+	// db初始化
+	dbUser := viper.GetString("Database.dbUser")
+	dbPass := viper.GetString("Database.dbPass")
+	dbHost := viper.GetString("Database.dbHost")
+	dbPort := viper.GetString("Database.dbPort")
+	dbname := viper.GetString("Database.dbname")
+	charset := viper.GetString("Database.charset")
+	db := model.InitDB(dbUser, dbPass, dbHost, dbPort, dbname, charset)
+	
+	defer db.Close()
+
+
 	r := gin.Default()
 
 	router.Router(r)
--- go-api/model/db.go
+++ go-api/model/db.go
@@ -3,12 +3,14 @@ package model
 import (
 	"database/sql"
 	"fmt"
+	"goapi/model/user"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 	"time"
 )
 
-func initDB(dbUser,dbPass,dbHost,dbPort,dbname,charset string) *sql.DB {
+func InitDB(dbUser,dbPass,dbHost,dbPort,dbname,charset string) *sql.DB {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
 		dbUser,
@@ -21,6 +23,16 @@ func initDB(dbUser,dbPass,dbHost,dbPort,dbname,charset string) *sql.DB {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
+	}
+
+	// 初始化user表
+	if ok := db.Migrator().HasTable(&user.User{}); ok {
+		log.Println("数据库中已经存在User表")
+	}else {
+		if err := db.AutoMigrate(&user.User{}); err != nil {
+			log.Println("初始化User失败")
+
+		}
 	}
 
 	sqlDB, err := db.DB()
@@ -36,7 +48,9 @@ func initDB(dbUser,dbPass,dbHost,dbPort,dbname,charset string) *sql.DB {
 }
 
 
-func isTableExist() {
+func isTableExist(db *gorm.DB, table *struct{}) bool {
+	if ok := db.Migrator().HasTable(&table); ok {
 
+	}
 }
 
