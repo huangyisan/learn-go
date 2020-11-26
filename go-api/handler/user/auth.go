2050bc7ace0bb4150a1178f59117834d17939c76
huangyisan <anonymousyisan@gmail.com>
2020-11-26 08:58:06 +0800

    update auth

--- go-api/handler/user/auth.go
+++ go-api/handler/user/auth.go
@@ -1,9 +1,24 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"goapi/model"
+	"goapi/model/user"
+)
 
 func Auth(c *gin.Context) {
+	db := model.GetDB()
+	username := c.PostForm("username")
+	//password := c.PostForm("password")
+
+	dbPassword, _ := db.Where("username = ?", username).Take(&user.User{}).Get("password")
+
+	//dbPassword.Get("password")
+	fmt.Println(dbPassword)
+
 	c.JSON(200, gin.H{
 		"message":"auth",
+		"dbPassword":dbPassword,
 	})
 }
