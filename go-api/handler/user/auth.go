757cfa935df87e12f1aab2d4184841e1fe8e5f09
huangyisan <anonymousyisan@gmail.com>
2020-11-27 21:43:48 +0800

    authen

--- go-api/handler/user/auth.go
+++ go-api/handler/user/auth.go
@@ -1,24 +1,34 @@
 package user
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"goapi/model"
 	"goapi/model/user"
+	"goapi/response"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Auth(c *gin.Context) {
 	db := model.GetDB()
+
+
 	username := c.PostForm("username")
-	//password := c.PostForm("password")
+	password := c.PostForm("password")
 
-	dbPassword, _ := db.Where("username = ?", username).Take(&user.User{}).Get("password")
+	var user user.User
+	db.Where("username = ?", username).Take(&user)
+	if user.ID == 0 {
+		response.Fail(c,"","不存在的用户")
+		return
+	}
 
-	//dbPassword.Get("password")
-	fmt.Println(dbPassword)
+	//比较密码
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		response.Fail(c,"","验证失败")
+		return
+	}
 
-	c.JSON(200, gin.H{
-		"message":"auth",
-		"dbPassword":dbPassword,
-	})
+	// 对比密码成功
+	response.Success(c, "","认证成功")
+
 }
