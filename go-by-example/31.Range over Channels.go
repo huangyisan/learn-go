5dc864dc329a9eb0e7d9f20680b08abc55057375
huangyisan <anonymousyisan@gmail.com>
2020-12-16 22:00:30 +0800

    timers

---
+++ go-by-example/31.Range over Channels.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// 使用range遍历channel
+// 保证channel是关闭的情况下进行遍历
+
+func main() {
+
+	ch1 := make(chan string, 2)
+	ch1 <- "string1"
+	ch1 <- "string2"
+
+	// 关闭后的channel才可以遍历，否则遍历完继续遍历就报错了
+	close(ch1)
+
+	// channel只有值，没有索引
+	for v := range ch1 {
+		fmt.Println(v)
+	}
+}
+
---
+++ go-by-example/32.Timers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	timer1 := time.NewTimer(time.Second * 2)
+
+	// .C 表示使用Time类型的channel
+	// 也就是2秒钟后出数据，等于等待2秒执行之后内容
+	// 这种使用等同time.sleep
+	<- timer1.C
+	fmt.Println("2s passed")
+
+	// 但是用NewTimer产生的定时器，可以在定时器失效前，用stop()取消他。
+	timer2 := time.NewTimer(time.Second * 3)
+
+	// 原本需要等待3s后执行的，被timer2.Stop()给停止了, 所以<- timer2.C就一直阻塞了，go协程下面就不会执行。
+	go func() {
+		<- timer2.C
+		fmt.Println("3s passed")
+	}()
+
+	timer2.Stop()
+	fmt.Println("stop timer")
+}
