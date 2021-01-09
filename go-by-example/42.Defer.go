faf684c3314b8a5fd781783e5300395ffcca7e61
huangyisan <anonymousyisan@gmail.com>
2021-01-09 14:05:16 +0800

    Defer

---
+++ go-by-example/42.Defer.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+// Defer 用于确保程序在执行完成后，会调用某个函数，一般是执行清理工作。 Defer 的用途跟其他语言的 ensure 或 finally 类似。
+
+func createFile(p string) *os.File {
+    fmt.Println("create")
+    f, err := os.Create(p)
+    if err != nil {
+        panic(err)
+    }
+    return f
+}
+
+func writeFile(f *os.File) {
+    fmt.Println("writing")
+    fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+    fmt.Println("closing")
+    err := f.Close()
+
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error: %v\n", err)
+        os.Exit(1)
+    }
+}
+
+func main() {
+    f := createFile("./42-Defer.txt")
+    // defer关闭
+    defer closeFile(f)
+    writeFile(f)
+}
