0d7767436b5cb1ab185bf8478405d922e17ee7ee
huangyisan <ahuangyisan@live.cn>
2021-04-27 20:54:41 +0800

    grpc server

--- learn-protobuf/cmd/server/main.go
+++ learn-protobuf/cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	pb "learn-protobuf/pb"
+	"learn-protobuf/service"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("hello world from serverm")
+	port := flag.Int("port", 8080, "the server port")
+	flag.Parse()
+	log.Printf("start server on port : %d", *port)
+
+	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	address := fmt.Sprintf("0.0.0.0:%d", *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal("can not start server ", err)
+
+	}
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
 }
