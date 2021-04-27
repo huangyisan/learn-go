27cb79bf79a59affb9d7ac6a539eeb8ec60633e7
huangyisan <ahuangyisan@live.cn>
2021-04-27 23:19:33 +0800

    add clinet

--- learn-protobuf/cmd/client/main.go
+++ learn-protobuf/cmd/client/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	pb "learn-protobuf/pb"
+	"learn-protobuf/sample"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 func main() {
-	fmt.Println("hello world from client")
+	serverAddress := flag.String("address", "", "the server address")
+	flag.Parse()
+	log.Printf("dial server %s", *serverAddress)
+
+	// grpc的方式和server建联
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal("can not dail server: ", err)
+	}
+
+	laptopClient := pb.NewLaptopServiceClient(conn)
+
+	laptop := sample.NewLaptop()
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			log.Print("laptop already exists")
+		} else {
+			log.Fatal("can not create laptop ", err)
+		}
+		// 有报错直接return停止
+		return
+	}
+	fmt.Printf("create new laptop with id: %s", res.Id)
 }
