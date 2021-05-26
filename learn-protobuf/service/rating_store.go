3787d889773e0bf78e654e2129c6a80c715ec653
huangyisan <anonymousyisan@gmail.com>
2021-05-26 20:54:12 +0800

    add rating store

---
+++ learn-protobuf/service/rating_store.go
@@ -0,0 +1,44 @@
+package service
+
+import "sync"
+
+type RatingStore interface {
+	Add(laptopID string, score float64) (*Rating, error)
+}
+
+type Rating struct {
+	Count uint32
+	Sum   float64
+}
+
+type InMemoryRatingStore struct {
+	mutex sync.RWMutex
+	// map以laptopID为key
+	rating map[string]*Rating
+}
+
+func NewInMemoryRatingStore() *InMemoryRatingStore {
+	return &InMemoryRatingStore{
+		rating: make(map[string]*Rating),
+	}
+}
+
+func (store *InMemoryRatingStore) Add(laptopID string, score float64) (*Rating, error) {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	rating := store.rating[laptopID]
+	if rating == nil {
+		rating = &Rating{
+			Count: 1,
+			Sum:   score,
+		}
+	} else {
+		rating.Count++
+		rating.Sum += score
+	}
+	// 将更新评分后的数据重新写入map
+	store.rating[laptopID] = rating
+	return rating, nil
+
+}
