07269610e928ce519d763bd23f0c679f881288d1
huangyisan <anonymousyisan@gmail.com>
2022-03-27 11:12:40 +0800

    update

--- the-way-to-go/ch5/main.go
+++ the-way-to-go/ch5/main.go
@@ -5,5 +5,3 @@ import "learn/functions"
 func main() {
 	functions.CallchangeSlice()
 }
-
-SELECT table_schema, ROUND(SUM(data_length+index_length)/1024/1024/1024,2) "size in GB" FROM information_schema.
