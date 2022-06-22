e3fbe57c5d168e37127b763dd48e0610691c9f54
huangyisan <anonymousyisan@gmail.com>
2022-06-22 10:20:53 +0800

    adaptee

---
+++ golang-design-pattern/结构型模式/adaptee/go.mod
@@ -0,0 +1,3 @@
+module adaptee
+
+go 1.17
---
+++ golang-design-pattern/结构型模式/adaptee/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	client := &client{}
+	mac := &mac{}
+
+	client.insertLightningConnectorIntoComputer(mac)
+
+	windowsMachine := &windows{}
+	windowsMachineAdapter := &windowsAdapter{windowMachine: windowsMachine}
+	client.insertLightningConnectorIntoComputer(windowsMachineAdapter)
+}
