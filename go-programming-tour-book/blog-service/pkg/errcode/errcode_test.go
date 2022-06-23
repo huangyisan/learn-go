1004bc9450eb1e6b07cac3ebf6e0cb0911ec2d8d
huangyisan <anonymousyisan@gmail.com>
2022-06-23 16:33:58 +0800

    update go-programming-tour-book

--- go-programming-tour-book/blog-service/pkg/errcode/errcode_test.go
+++ go-programming-tour-book/blog-service/pkg/errcode/errcode_test.go
@@ -1,1 +1,10 @@
 package errcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_abc(t *testing.T) {
+	fmt.Println("abc")
+}
--- go-programming-tour-book/blog-service/pkg/setting/setting.go
+++ go-programming-tour-book/blog-service/pkg/setting/setting.go
@@ -1,1 +1,42 @@
 package setting
+
+import (
+	"github.com/spf13/viper"
+	"sync"
+)
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSetting() (*Setting, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")
+	vp.AddConfigPath("configs/")
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	return &Setting{vp}, nil
+}
+
+type messagePool struct {
+	pool *sync.Pool
+}
+type Message struct {
+	Count int
+}
+
+var msgPool = &messagePool{
+	// 如果消息池里没有消息，则新建一个Count值为0的Message实例
+	pool: &sync.Pool{
+		New: func() interface{} {
+			return &Message{Count: 0}
+		},
+	},
+}
+
+func (m *messagePool) AddMsg(msg *Message) {
+	m.pool.Put(msg)
+}
