9065e611e865a77d4cf9ae6c6dd95740c29ebaad
huangyisan <anonymousyisan@gmail.com>
2022-06-24 18:02:28 +0800

    update

--- go-etherem-tutorial/01_client/01.client.go
+++ go-etherem-tutorial/01_client/01.client.go
@@ -14,4 +14,12 @@ func main() {
 	}
 	fmt.Println("we have a connection")
 	_ = client
+
+	client1, err := ethclient.Dial("https://u0lvw6c3lk:7vUkST4yxgKT_yjF5laCagbtYNCkw0ZOQIYv-FteCvE@u0avnfe4vh-u0j18z3adv-rpc.us0-aws.kaleido.io/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("we have a connection")
+	_ = client1
+
 }
