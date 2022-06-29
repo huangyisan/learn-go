5aeb07a67d40d505475e424c68465166aad567b0
huangyisan <anonymousyisan@gmail.com>
2022-06-29 15:33:41 +0800

    update

--- go-etherem-tutorial/07_Utilities/01_CollectionOfUtilityFunctions.go
+++ go-etherem-tutorial/07_Utilities/01_CollectionOfUtilityFunctions.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-)
-
 func main() {
+	// https://goethereumbook.org/en/util-go/
 	// 验证是否有效以太坊地址util.IsValidAddress
-	valid := hexutil.IsValidAddress("0x323b5d4c32345ced77393b3530b1eed0f346429d")
-	fmt.Println(valid) // true
+	//valid := hexutil.IsValidAddress("0x323b5d4c32345ced77393b3530b1eed0f346429d")
+	//fmt.Println(valid) // true
 
 }
